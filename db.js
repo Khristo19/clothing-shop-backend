261dbb2758c6fd31f461b5432cd65cd6110df7f6
Khristo19 <khristo.nachkebia@gmail.com>
2025-07-14 01:28:55 +0400

    Fix: clean Pool config for Supabase IPv4 connection

--- db.js
+++ db.js
@@ -5,9 +5,7 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
-    },
-    host: 'db.xxzmnjkwlxpkfrrznnmp.supabase.co', // force IPv4
-    port: 5432
+    }
 });
 
 module.exports = pool;
