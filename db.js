3df780def79b90695d11f77547104ea14cf130f7
Khristo19 <khristo.nachkebia@gmail.com>
2025-07-14 01:52:17 +0400

    Force IPv4 by explicitly setting Supabase host

--- db.js
+++ db.js
@@ -5,7 +5,8 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
-    }
+    },
+    host: 'db.xxzmnjkwlxpkfrrznnmp.supabase.co' // <- this forces IPv4 lookup
 });
 
 module.exports = pool;
