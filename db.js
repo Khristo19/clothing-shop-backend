41011466388a5838a5786952be12ec88a687c300
Khristo19 <khristo.nachkebia@gmail.com>
2025-07-14 04:58:41 +0400

    Fix: move login.js to api/auth

--- db.js
+++ db.js
@@ -1,9 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
 });
 
 module.exports = pool;
+
+console.log('[DB INIT] DATABASE_URL =', process.env.DATABASE_URL);
