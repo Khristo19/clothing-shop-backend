9ed489aca9f103e8410f44ebf8ccf964d6892794
Khristo19 <khristo.nachkebia@gmail.com>
2025-07-14 04:54:40 +0400

    Fix: move login.js to api/auth

--- db.js
+++ db.js
@@ -1,12 +1,6 @@
 const { Pool } = require('pg');
-require('dotenv').config();
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'Xristo1080.',
-    host: 'db.xxzmnjkwlxpkfrrznnmp.supabase.co', // IPv4 host
-    port: 5432,
-    database: 'postgres',
     ssl: {
         rejectUnauthorized: false
     }
